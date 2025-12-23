35d71b224e318b25fa4088d741a2ac9c7a22ecea
onimtadev08 <dev8@onimtait.com>
2025-12-23 09:40:16 +0530

    version one

--- App.tsx
+++ App.tsx
@@ -1,27 +1,16 @@
+// App.tsx
 import React from 'react';
-import {StatusBar, useColorScheme} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
-import SplashScreen from './src/screens/SplashScreen';
-import {RootStackParamList} from './src/types/navigation';
-import LoginScreen from './src/screens/LoginScreen';
-import DashboardScreen from './src/screens/DashboardScreen';
-
-const Stack = createNativeStackNavigator<RootStackParamList>();
+import { StatusBar, useColorScheme } from 'react-native';
+import AppNavigator from './src/navigation/AppNavigator';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <NavigationContainer>
+    <>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="SplashScreen" component={SplashScreen} />
-        <Stack.Screen name="LoginScreen" component={LoginScreen} />
-        <Stack.Screen name="DashboardScreen" component={DashboardScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <AppNavigator />
+    </>
   );
 }
 
